f08be46220a8e74dbea0fdca5ee11a4fa4153d42
Ezequiel <ezequiel.luna1988@gmail.com>
2020-01-03 20:25:27 -0300

    AnotadorActivity creada

--- app/src/main/java/com/example/anotadoruno/MainActivity.java
+++ app/src/main/java/com/example/anotadoruno/MainActivity.java
@@ -2,13 +2,36 @@ package com.example.anotadoruno;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.button.MaterialButton;
+
+import butterknife.BindView;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.botonAnotadorMainActivity)
+    MaterialButton botonAnotadorMainActivity;
+    @BindView(R.id.botonHistorialMainActivity)
+    MaterialButton botonHistorialMainActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        clickButtonAnotador();
+    }
+
+    private void clickButtonAnotador() {
+        botonAnotadorMainActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AnotadorActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
