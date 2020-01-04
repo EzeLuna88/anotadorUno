13e9ff774e7416afe6afa23559bf870f31207ff6
Ezequiel <ezequiel.luna1988@gmail.com>
2020-01-03 22:05:46 -0300

    fragments comunicados

--- app/src/main/java/com/example/anotadoruno/AnotadorActivity.java
+++ app/src/main/java/com/example/anotadoruno/AnotadorActivity.java
@@ -1,14 +1,33 @@
 package com.example.anotadoruno;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
-public class AnotadorActivity extends AppCompatActivity {
+import com.example.anotadoruno.fragments.AmistosoCrearJugadoresFragment;
+import com.example.anotadoruno.fragments.GrupoOAmistosoFragment;
+
+public class AnotadorActivity extends AppCompatActivity implements GrupoOAmistosoFragment.NotificadorBotonAmistoso {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_anotador);
+
+        GrupoOAmistosoFragment grupoOAmistosoFragment = new GrupoOAmistosoFragment();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.contenedorDeFragment, grupoOAmistosoFragment).commit();
+    }
+
+    @Override
+    public void hicieronClickBotonAmistoso() {
+    AmistosoCrearJugadoresFragment amistosoCrearJugadoresFragment = new AmistosoCrearJugadoresFragment();
+    FragmentManager fragmentManager = getSupportFragmentManager();
+    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+    fragmentTransaction.replace(R.id.contenedorDeFragment, amistosoCrearJugadoresFragment).commit();
+
     }
 }
--- app/src/main/java/com/example/anotadoruno/fragments/GrupoOAmistosoFragment.java
+++ app/src/main/java/com/example/anotadoruno/fragments/GrupoOAmistosoFragment.java
@@ -1,9 +1,9 @@
-package com.example.anotadoruno;
+package com.example.anotadoruno.fragments;
 
 import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -11,11 +11,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import com.example.anotadoruno.R;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 
-public class EleccionGrupoFragment extends Fragment {
+public class GrupoOAmistosoFragment extends Fragment {
 
     @BindView(R.id.botonCrearGrupoEleccionFragment)
     Button botonCrearGrupoEleccionFragment;
@@ -23,20 +25,27 @@ public class EleccionGrupoFragment extends Fragment {
     Button botonElegirGrupoEleccionFragment;
     @BindView(R.id.botonAmistosoEleccionFragment)
     Button botonAmistosoEleccionFragment;
+    private NotificadorBotonAmistoso notificadorBotonAmistoso;
 
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        this.notificadorBotonAmistoso = (NotificadorBotonAmistoso) context;
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_eleccion_grupo, container, false);
+        View view = inflater.inflate(R.layout.fragment_grupo_o_amistoso, container, false);
 
         ButterKnife.bind(this, view);
 
         botonAmistosoEleccionFragment.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+            notificadorBotonAmistoso.hicieronClickBotonAmistoso();
             }
         });
 
@@ -44,4 +53,7 @@ public class EleccionGrupoFragment extends Fragment {
     }
 
 
+    public interface NotificadorBotonAmistoso {
+        public void hicieronClickBotonAmistoso();
+    }
 }
